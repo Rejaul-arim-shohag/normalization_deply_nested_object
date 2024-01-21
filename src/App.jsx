22d6a135f432f24eebdeb8bf7eeddaf52aa52217
Rejaul karim <76891535+Rejaul-arim-shohag@users.noreply.github.com>
2024-01-21 23:19:44 +0600

    array normalaization

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from "react"
-import { WithNestedObject } from "./Componets/WIthNestedObject"
-
+import { useState } from "react";
+import { WithNestedObject } from "./Componets/WIthNestedObject";
+import TravelPlan from "./Componets/TravelPlan";
 
 function App() {
   return (
     <>
-      <WithNestedObject />
+      {/* <WithNestedObject /> */}
+      <TravelPlan />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
