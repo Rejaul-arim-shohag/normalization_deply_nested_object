0ddd28daf044770f32c07a9a7008d29cbe2b9611
Rejaul karim <76891535+Rejaul-arim-shohag@users.noreply.github.com>
2024-01-21 18:55:26 +0600

    first commit

---
+++ src/Componets/WIthNestedObject.jsx
@@ -0,0 +1,10 @@
+import TravelPlan from "./TravelPlan"
+
+export const WithNestedObject = () => {
+    return (
+        <>
+            <h1>With Nested Object</h1>
+            <TravelPlan />
+        </>
+    )
+}
